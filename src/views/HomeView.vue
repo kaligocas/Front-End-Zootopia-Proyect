4e0f47e8497eb17601a0d09410ce8e0234003275
Alicia González Castro <aliglezcastro@gmail.com>
2024-01-17 13:33:03 +0100

    footer links

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,9 @@
 <script setup lang="ts">
-import Navbar from '@/components/header/navbar.vue';
-
-
 
 </script>
 
 <template>
   <main>
-    <Navbar/>
-    <h1>Hello probando otra cosa otra prueba otra vez</h1>
+    
   </main>
 </template>
