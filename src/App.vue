9c0cca105f8be2fb11630bca2af665f8d6ec052d
Stiven Diaz <steven_el22@hotmail.com>
2024-01-18 14:21:34 -0800

    cards finish

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@ import { RouterLink, RouterView } from 'vue-router'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/family">Family</RouterLink>
       </nav>
     </div>
   </header>
---
+++ src/views/FamilyView.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import CardFamily from '@/components/family/CardFamily.vue';
+</script>
+
+<template>
+  <main>
+    <h1>Familia</h1>
+
+    <section class="Galery">
+
+     <CardFamily/>
+
+    </section>
+  </main>
+
+</template>
+
+<style scoped>
+
+h1{
+  text-align: center;
+}
+
+.Galery{
+display: grid;
+justify-items: center;
+align-items: center;
+grid-template-columns: repeat(auto-fit,minmax(180px,1fr));
+gap: 30px;
+}
+
+
+</style>
